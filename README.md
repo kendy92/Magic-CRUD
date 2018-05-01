9c20408eae7738a7020d07b733b8f87ce687cea5
kendy92 <dinhconganh@gmail.com>
2018-05-01 01:49:54 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,62 @@
-# Magic-CRUD
-The framework that allows you to do CRUD in the shortest way and applicable to all php application. It will save you lot of time when building your own custom CMS
+#MAGIC CRUD
+
+**HOW TO USE**
+
+- Require the magic crud  in the file you want to use by adding the following line of code:
+
+
+
+>  require_once "magic_crud.php";
+
+Config your database information in magic_crud.php file.
+
+
+**INSERT ROW**
+
+- Create an object that holds up column name and value which require to add to the database. Result will return number of rows that successfully add to database. 0 if no row added!
+
+> \$new_item = array('col_name_1' => 'value_1', 	   col_name_2' =>
+> 'value_2');
+
+- Call the function and pass the object and table name to it.
+
+> MAGIC_CRUD::insert(\$new_item, "your_table_name");
+
+
+**UPDATE EXISTING ROW**
+
+- Create an object that holds up column name and value that require to update to the database. Result will return number of rows that successfully add to database. 0 if no row added!
+
+> \$new_item = array('col_name_1' => 'value_1', col_name_2' =>
+> 'value_2');
+
+- Call the function and pass the object, table name, condition column and condition value to it.
+
+> MAGIC_CRUD::update($obj, $table_name, $condition_col, $condition_val);
+
+**DELETE EXISTING ROW**
+
+- Call the function and pass the id of existing row and table name to delete from database. Result will return number of rows that successfully add to database. 0 if no row added!
+
+> MAGIC_CRUD::delete($id, $table_name);
+
+**FIND BY ID**
+
+- Call the function and pass the id of existing row and table name to show data regarding to that id from database. Result will return as an object and use foreach loop to loop through the result.
+
+> MAGIC_CRUD::details($id, $table_name)
+
+**SHOW DATA**
+
+- Call the function and pass the table name to show all data regarding to that table. Result will be sorted in descending order by default but you can pass second parameter as "ASC" to sort it in ascending order and return as an object. Use foreach loop to loop through the result.
+
+> MAGIC_CRUD::show(\$table_name,$order = "DESC");
+
+**CUSTOM SHOW**
+
+- Call the function and pass the custom query. Result will return as an object and use foreach loop to loop through the result. It is ideally to use for multiple join table statement.
+
+> MAGIC_CRUD::custom_show($query);
+
+
+**CODED BY LILCASOFT.INFO**
---
+++ magic_crud.php
@@ -0,0 +1,152 @@
+<?php
+/*
+MAGIC CRUD 2018 VERSION 1.0
+CODED BY LILCASOFT.INFO
+*/
+class MAGIC_CRUD{
+
+  /* DATABASE CONFIGURATION */
+  private static $host = "mysql:host=localhost; dbname=you_db_name"; //put your database name here
+  private static $user = "root"; //change username to match your sql user
+  private static $pass = "root"; //change password to match your sql pass
+
+  /* ARRAY TO STORE COLS, VALS DECLARATION */
+  private static $cols = "";
+  private static $vals = "";
+
+
+  public static function connectDB() { //USE: self::connectDB() to initialize connection
+    try {
+      $conn = new PDO(self::$host, self::$user, self::$pass);
+      $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch (PDOException $e) {
+      return $e->getMessage();
+    }
+    return $conn;
+  }
+
+  public static function custom_show($query) { //inject custom query to the function. Use for join $query
+    try {
+      $conn = self::connectDB();
+      $conn->query("SET NAMES utf8");
+      $cmd = $conn->prepare($query);
+      $cmd->setFetchMode(PDO::FETCH_OBJ); //return result as an object
+      $cmd->execute();
+      return $cmd->fetchAll();
+    } catch (PDOException $e) {
+      return $e->getMessage();
+    }
+  }
+
+  public static function show($table_name,$order = "DESC"){ /* $order = {DESC | ASC} */
+    try {
+      $conn = self::connectDB();
+      $conn->query("SET NAMES utf8");
+      $cmd = $conn->prepare("SELECT * FROM {$table_name} ORDER BY id {$order}");
+      $cmd->setFetchMode(PDO::FETCH_OBJ); //return result as an object
+      $cmd->execute();
+      return $cmd->fetchAll();
+    } catch (PDOException $e) {
+      return $e->getMessage();
+    }
+  }
+
+  public static function insert($obj,$table_name){ /* Pass in object contains column name and value */
+    $count = 1;
+    $query = "";
+    $arr_cols = array();
+    $arr_vals = array();
+    try {
+      $conn = self::connectDB();
+
+      $query .= "INSERT INTO ".$table_name;
+      foreach($obj as $key => $val) {
+        if($count == count($obj)){ //remove hyphon at last item of array
+          self::$cols .= $key;
+          self::$vals .= ":".$key;
+
+        }else{
+          self::$cols .= $key.", ";
+          self::$vals .= ":".$key.", ";
+        }
+        array_push($arr_cols,$key);
+        array_push($arr_vals,$val);
+        $count++;
+      }
+      $query = $query."(".self::$cols.") VALUES(".self::$vals.")";
+      $cmd = $conn->prepare($query);
+
+      for ($i=0; $i < count($obj); $i++) { //bind var to all items
+        $cmd->bindParam(":{$arr_cols[$i]}", $arr_vals[$i]);
+      }
+
+      return $cmd->execute();
+
+    } catch (PDOException $e) {
+      return $e->getMessage();
+    }
+
+  }
+
+  public static function update($obj, $table_name, $condition_col, $condition_val){
+    $count = 1;
+    $query = "";
+    $arr_cols = array();
+    $arr_vals = array();
+    try {
+      $conn = self::connectDB();
+
+      $query .= "UPDATE ".$table_name." SET ";
+      foreach($obj as $key => $val) {
+        if($count == count($obj)){ //remove hyphon at last item of array
+          self::$cols .= $key."= :{$key}";
+
+        }else{
+          self::$cols .= $key."= :{$key}, ";
+        }
+        array_push($arr_cols,$key);
+        array_push($arr_vals,$val);
+        $count++;
+      }
+      $query = $query.self::$cols." WHERE ".$condition_col."= :".$condition_col;
+      $cmd = $conn->prepare($query);
+
+      for ($i=0; $i < count($obj); $i++) { //bind var to all items
+        $cmd->bindParam(":{$arr_cols[$i]}", $arr_vals[$i]);
+      }
+        //set param for condition statement
+        $cmd->bindParam(":{$condition_col}", $condition_val);
+      return $cmd->execute();
+
+    } catch (PDOException $e) {
+      return $e->getMessage();
+    }
+  }
+
+  public static function delete($id, $table_name){
+    try {
+      $conn = self::connectDB();
+      $cmd = $conn->prepare("DELETE FROM {$table_name} WHERE id = :i");
+      $cmd->bindParam(':i',$id);
+      return $cmd->execute();
+    } catch (PDOException $e) {
+      return $e->getMessage();
+    }
+  }
+
+  public static function details($id, $table_name){
+    try {
+      $conn = self::connectDB();
+      $conn->query("SET NAMES utf8");
+      $cmd = $conn->prepare("SELECT * FROM {$table_name} WHERE id = :i");
+      $cmd->setFetchMode(PDO::FETCH_OBJ); //return result as an object
+      $cmd->bindParam(':i',$id);
+      $cmd->execute();
+      return $cmd->fetchAll();
+    } catch (PDOException $e) {
+      return $e->getMessage();
+    }
+  }
+
+}
+ ?>
