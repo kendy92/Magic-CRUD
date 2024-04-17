969f4b9c30a3086db3dbb1558024714fbaf59077
Lilca Dinh <dinhconganh@gmail.com>
2024-04-17 13:20:24 -0400

    Update magic_crud.php
    allow to perform multiple insert, edit query.

--- magic_crud.php
+++ magic_crud.php
@@ -51,42 +51,81 @@ class MAGIC_CRUD{
     }
   }
 
-  public static function insert($obj,$table_name){ /* Pass in object contains column name and value */
-    $count = 1;
-    $query = "";
-    $arr_cols = array();
-    $arr_vals = array();
-    try {
-      $conn = self::connectDB();
+  public static function insert($obj, $table_name){
+			$count = 1;
+			$query = "";
+			$arr_cols = array();
+			$arr_vals = array();
+			try {
+					$conn = self::connectDB();
+					$cols = ""; // Initialize $cols variable
+					$vals = ""; // Initialize $vals variable
 
-      $query .= "INSERT INTO `".$table_name."`";
-      foreach($obj as $key => $val) {
-        if($count == count($obj)){ //remove hyphon at last item of array
-          self::$cols .= $key;
-          self::$vals .= ":".$key;
+					$query .= "INSERT INTO `".$table_name."`";
+					foreach($obj as $key => $val) {
+							if($count == count($obj)){ //remove hyphen at last item of array
+									$cols .= $key;
+									$vals .= ":".$key;
+							}else{
+									$cols .= $key.", ";
+									$vals .= ":".$key.", ";
+							}
+							array_push($arr_cols, $key);
+							array_push($arr_vals, $val);
+							$count++;
+					}
+					$query = $query."(".$cols.") VALUES(".$vals.")";
+					$cmd = $conn->prepare($query);
+					for ($i=0; $i < count($obj); $i++) { //bind var to all items
+							$cmd->bindParam(":{$arr_cols[$i]}", $arr_vals[$i]);
+					}
+					return $cmd->execute();
+			} catch (PDOException $e) {
+					return $e->getMessage();
+			}
+	}
 
-        }else{
-          self::$cols .= $key.", ";
-          self::$vals .= ":".$key.", ";
-        }
-        array_push($arr_cols,$key);
-        array_push($arr_vals,$val);
-        $count++;
-      }
-      $query = $query."(".self::$cols.") VALUES(".self::$vals.")";
-      $cmd = $conn->prepare($query);
 
-      for ($i=0; $i < count($obj); $i++) { //bind var to all items
-        $cmd->bindParam(":{$arr_cols[$i]}", $arr_vals[$i]);
-      }
+	public static function insert_get_id($obj, $table_name){
+			$count = 1;
+			$query = "";
+			$arr_cols = array();
+			$arr_vals = array();
+			try {
+					$conn = self::connectDB();
+					$cols = ""; // Initialize $cols variable
+					$vals = ""; // Initialize $vals variable
 
-      return $cmd->execute();
+					$query .= "INSERT INTO `".$table_name."`";
+					foreach($obj as $key => $val) {
+							if($count == count($obj)){ //remove hyphen at last item of array
+									$cols .= $key;
+									$vals .= ":".$key;
+							}else{
+									$cols .= $key.", ";
+									$vals .= ":".$key.", ";
+							}
+							array_push($arr_cols, $key);
+							array_push($arr_vals, $val);
+							$count++;
+					}
+					$query = $query."(".$cols.") VALUES(".$vals.")";
+					$cmd = $conn->prepare($query);
+					for ($i=0; $i < count($obj); $i++) { //bind var to all items
+							$cmd->bindParam(":{$arr_cols[$i]}", $arr_vals[$i]);
+					}
+					$result = $cmd->execute();
 
-    } catch (PDOException $e) {
-      return $e->getMessage();
-    }
-
-  }
+					if ($result) {
+							// If insertion was successful, return the last inserted row id
+							return $conn->lastInsertId();
+					} else {
+							return false;
+					}
+			} catch (PDOException $e) {
+					return $e->getMessage();
+			}
+	}
 
   public static function update($obj, $table_name, $condition_col, $condition_val){
     $count = 1;
@@ -95,7 +134,8 @@ class MAGIC_CRUD{
     $arr_vals = array();
     try {
       $conn = self::connectDB();
-
+      $cols = ""; // Initialize $cols variable
+			$vals = ""; // Initialize $vals variable
       $query .= "UPDATE `".$table_name."` SET ";
       foreach($obj as $key => $val) {
         if($count == count($obj)){ //remove hyphon at last item of array
